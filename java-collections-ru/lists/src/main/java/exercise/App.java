746c197b344d848375cfd4b27797e1d2e3e1bbb5
@hexlet/cli <support@hexlet.io>
2023-07-23 21:13:18 +0200

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -5,5 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 // BEGIN
-
+public class App {
+    public static boolean scrabble(String strSymbols, String word) {
+        List<String> symbols = new ArrayList<>(Arrays.asList(strSymbols.split("")));
+        for (String letter: word.split("")) {
+            if (!symbols.remove(letter.toLowerCase())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 //END
